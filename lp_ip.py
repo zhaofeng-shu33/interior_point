4784b9e9113afdffd6ef1e34cd50e05bffe33bb4
zhaofeng-shu33 <616545598@qq.com>
2021-05-13 11:16:11 +0800

    add primal dual

--- lp_ip.py
+++ lp_ip.py
@@ -57,3 +57,38 @@ def lp_ip(A, b, c, y0=None, eps=1e-4, mu=2):
             t *= mu
     return (y, np.dot(b, y))
 
+def lp_ip_pd(A, b, c, eps=1e-2, mu=0.5):
+    '''
+    primal dual method
+
+    Returns
+    -------
+    argmax_y: array-like
+    max_y: float
+    '''
+    m, n = A.shape
+    x = np.ones(n)
+    s = np.ones(n)
+    y = np.zeros(m)
+    F_diff = np.zeros([2 * n + m, 2 * n + m])
+    F_diff[:n, n:(m + n)] = A.T
+    F_diff[n:(m + n), :n] = A
+    F_diff[:n, (m + n): (2 * n + m)] = - np.eye(n)
+    err = 1
+    while err > eps:
+        sigma = mu * np.dot(x, s) / n
+        F_1 = A @ y + s - c
+        F_2 = A @ x - b
+        F_3 = x * s - sigma
+        F = np.array([F_1, F_2, F_3])
+        err = sigma + np.linalg.norm(F)
+        if err < eps:
+            break
+        np.fill_diagonal(F_diff[(m + n):,:n], s)
+        np.fill_diagonal(F_diff[(m + n):,(m + n)], x)
+        packed_sol = np.linalg.solve(F_diff, -F)
+        # always update by unit length
+        x += packed_sol[:n]
+        y += packed_sol[n:(m + n)]
+        s += packed_sol[(m + n):]
+    return (y, np.dot(b, y))
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 
 import numpy as np
-from lp_ip import lp_ip
+from lp_ip import lp_ip, lp_ip_pd
 class LP_IP(unittest.TestCase):
     def test_basic(self):
         b = np.array([-1, -1], dtype=float)
@@ -17,6 +17,20 @@ class LP_IP(unittest.TestCase):
         c = np.array([4, 8, 6, 0, 0, 0], dtype=float)
         y0 = np.array([0.5, 0.5, 0.5]) # exact interior point
         argmax_y, max_y = lp_ip(A, b, c, y0=y0)
+
+class LP_IP_PD(unittest.TestCase):
+    def test_basic(self):
+        b = np.array([-1, -1], dtype=float)
+        A = np.array([[1, 2, -1, 0], [2, 1, 0, -1]], dtype=float)
+        c = np.array([1, 1, 0, 0], dtype=float)
+        argmax_y, max_y = lp_ip_pd(A, b, c)
+        self.assertTrue(np.linalg.norm(argmax_y) < 1e-3)
+        self.assertTrue(np.abs(max_y) < 1e-3)
+    def test_vineyard(self):
+        b = np.array([3, 4, 2], dtype=float)
+        A = np.array([[2, 1, 0, -1, 0, 0], [0, 0, 3, 0, -1, 0], [0, 2, 1, 0, 0, -1]], dtype=float)
+        c = np.array([4, 8, 6, 0, 0, 0], dtype=float)
+        argmax_y, max_y = lp_ip_pd(A, b, c)
 
 if __name__ == '__main__':
     unittest.main()
